a36823deecdd10ff27280d16f5ecba895484a6ea
Jose Vicente Espinosa Amaro <josevicente.espinosa@broadcom.com>
2020-11-27 09:58:59 +0100

    Adding exercise explanation

--- class03/column_name_number.sh
+++ class03/column_name_number.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create a script that will return column names together with their column number from the csv files. The
+# first argument should be file name and the second delimiter.
+
 FILENAME=$1
 DELIMITER=$2
 
